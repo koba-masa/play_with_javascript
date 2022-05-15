1414dc66bf542fe3e60fe38c104e0df725aa8166
koba-masa <kobayashi_mshr@koba-masa.com>
2022-05-15 22:52:04 +0900

    feat: 関数をクラスに変換する
    https://ja.reactjs.org/docs/state-and-lifecycle.html#converting-a-function-to-a-class

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -5,14 +5,20 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
 function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  root.render(element);
+  root.render(<Clock date={new Date()} />);
 }
 
 setInterval(tick, 1000);
