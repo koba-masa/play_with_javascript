0be99948ac5fab686b175be2e5e1ad8ec696fa8d
koba-masa <kobayashi_mshr@koba-masa.com>
2022-05-15 21:20:47 +0900

    feat: コンポーネントのレンダー
    https://ja.reactjs.org/docs/components-and-props.html#rendering-a-component

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -4,21 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
-
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  root.render(element);
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-setInterval(tick, 1000);
+const element = <Welcome name="Sara" />;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
