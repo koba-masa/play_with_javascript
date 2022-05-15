a1beed9724996f840f711295a8def81663bd16df
koba-masa <kobayashi_mshr@koba-masa.com>
2022-05-15 21:31:36 +0900

    feat: コンポーネントの抽出(1)
    https://ja.reactjs.org/docs/components-and-props.html#extracting-components

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -3,21 +3,42 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
+
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-function App() {
+function Comment(props) {
   return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
+    <div className="Comment">
+      <div className="UserInfo">
+        <img className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
     </div>
   );
 }
 
-const element = <App />;
+const element = <Comment date={comment.date} text={comment.text} author={comment.author} />;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(element);
 
