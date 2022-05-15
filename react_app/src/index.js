519600e146695f0b5ac2258a570d9e30508a7a3a
koba-masa <kobayashi_mshr@koba-masa.com>
2022-05-15 22:58:20 +0900

    feat: クラスにローカルな state を追加する
    https://ja.reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-class

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -7,18 +7,23 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
 }
 
 function tick() {
-  root.render(<Clock date={new Date()} />);
+  root.render(<Clock />);
 }
 
 setInterval(tick, 1000);
