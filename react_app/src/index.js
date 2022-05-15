75efc1de1db35adb006d61c4d87bcf832c8f220b
koba-masa <kobayashi_mshr@koba-masa.com>
2022-05-15 20:32:52 +0900

    feat: 要素を DOM として描画する
    https://ja.reactjs.org/docs/rendering-elements.html#rendering-an-element-into-the-dom

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const element = <h1>Hello, world</h1>;
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
 );
+root.render(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
