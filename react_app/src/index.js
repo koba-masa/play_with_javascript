9b6793d7edc02a56ab7b97ff8734938862a03d25
koba-masa <kobayashi_mshr@koba-masa.com>
2022-05-15 20:36:25 +0900

    feat: レンダーされた要素の更新
    https://ja.reactjs.org/docs/rendering-elements.html#updating-the-rendered-element

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const element = <h1>Hello, world</h1>;
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
-root.render(element);
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);
+}
+
+setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
